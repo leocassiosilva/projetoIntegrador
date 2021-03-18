8d444053b563d9fa3cf19b40ba16734346c1c56c
Felipe Gabriel <fgabrielqr@gmail.com>
2021-03-18 19:06:21 -0300

    Teste Cadastro Produto

--- product/tests/tests_view.py
+++ product/tests/tests_view.py
@@ -1,1 +1,14 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+# Teste de unitario cadastro de produto
+
+# teste para view de cadastro de produto
+class ProdutoViewTestCase(TestCase):
+
+    def test_cadastroProduto(self):
+        response = self.client.get(reverse('cadastrar'))
+        self.assertEquals(response.status_code, 200)
+
 
