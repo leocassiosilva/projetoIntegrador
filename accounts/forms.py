d3810a850ae762cca9c72fb02e2b40636697c948
leocassiosilva <leocassiosilva1234@gmail.com>
2021-03-23 08:11:29 -0300

    envio de email

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from rolepermissions.roles import assign_role
 from .models import CustomUsuario
 
@@ -6,7 +8,8 @@ from .models import CustomUsuario
 class CustomUsuarioCriarForm(UserCreationForm):
     class Meta:
         model = CustomUsuario
-        fields = ('username', 'first_name', 'last_name', 'telefone', 'cep', 'cidade', 'rua', 'bairro', 'logadouro', 'numero')
+        fields = (
+        'username', 'first_name', 'last_name', 'telefone', 'cep', 'cidade', 'rua', 'bairro', 'logadouro', 'numero')
 
     def save(self, commit=True):
         user = super().save(commit=False)
@@ -16,12 +19,23 @@ class CustomUsuarioCriarForm(UserCreationForm):
         if commit:
             user.save()
             assign_role(user, 'cliente')
+            data = {'usuario': user, }
+            plain_text = render_to_string('accounts/emails/email.txt', data)
+            html_email = render_to_string('accounts/emails/email.html', data)
+            send_mail("Coma Bem - Seja Bem Vindo!",
+                      plain_text,
+                      "comabemrn@gmail.com",
+                      ['{0}'.format(user)],
+                      html_message=html_email
+                      )
         return user
+
 
 class FormVendedor(UserCreationForm):
     class Meta:
         model = CustomUsuario
-        fields = ('username', 'first_name', 'last_name', 'telefone', 'cep', 'cidade', 'rua', 'bairro', 'logadouro', 'numero')
+        fields = (
+        'username', 'first_name', 'last_name', 'telefone', 'cep', 'cidade', 'rua', 'bairro', 'logadouro', 'numero')
 
     def save(self, commit=True):
         user = super().save(commit=False)
@@ -31,4 +45,13 @@ class FormVendedor(UserCreationForm):
         if commit:
             user.save()
             assign_role(user, 'produtor')
+            data = {'usuario': user, }
+            plain_text = render_to_string('accounts/emails/email.txt', data)
+            html_email = render_to_string('accounts/emails/email.html', data)
+            send_mail("Coma Bem - Seja Bem Vindo!",
+                      plain_text,
+                      "comabemrn@gmail.com",
+                      ['{0}'.format(user)],
+                      html_message=html_email
+                      )
         return user
--- projetoIntegrador/settings.py
+++ projetoIntegrador/settings.py
@@ -151,6 +151,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = 'Nome <sigoorientacoes@gmail.com>'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'sigoorientacoes@gmail.com'
+EMAIL_HOST_USER = 'comabemrn@gmail.com'
 EMAIL_HOST_PASSWORD = '#'
 EMAIL_PORT = 587
