931cef972eee12711603757d170bf49312cc3157
fgabrielqr <fgabrielqr@gmail.com>
2021-03-28 16:01:07 -0300

    Tirando erros

--- pedidos/models.py
+++ pedidos/models.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from checkout.models import CarrinhoItem
 
 class PedidoManager(models.Manager):
-
     def create_order(self, usuario, cart_items):
         pedido = self.create(usuario=usuario)
         for cart_item in cart_items:
@@ -12,18 +11,6 @@ class PedidoManager(models.Manager):
                 preco=cart_item.preco
             )
         return pedido
-
-class PedidoManage(models.Model):
-
-    def create_pedido(self, user, carrinho_items):
-        pedido = self.create(User=user)
-        for carrinho_item in carrinho_items:
-            pedido_item = PedidoItem.objects.create(
-                pedido=pedido, quantidade=carrinho_item.quantidade, product=carrinho_item.product,
-                preco=carrinho_item.preco
-            )
-        return pedido
-
 
 class Pedido(models.Model):
     STATUS_CHOICES = (
@@ -37,14 +24,7 @@ class Pedido(models.Model):
     data_criacao = models.DateTimeField('Criado em', auto_now_add=True)
     data_modificacao = models.DateTimeField('Modificado em', auto_now=True)
 
-<<<<<<< HEAD
     objects = PedidoManager()
-=======
-    objects = PedidoManage()
-
-    def __str__(self):
-        return 'Pedido #{}'.format(self.pk)
->>>>>>> origin/main
 
     class Meta:
         verbose_name = 'Pedido'
