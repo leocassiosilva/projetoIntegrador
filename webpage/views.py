5d1954157b442e7fd520dd8444aa8a87775400b3
Felipe Gabriel <fgabrielqr@gmail.com>
2021-03-29 12:08:20 -0300

    Atualizando Template

--- webpage/views.py
+++ webpage/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from accounts.models import CustomUsuario
 
 
@@ -8,5 +9,6 @@ def home(request):
     return render(request, 'home.html')
 
 
-class IndexView(TemplateView):
+class IndexView(LoginRequiredMixin, TemplateView):
+    login_url = reverse_lazy('login')
     template_name = 'index.html'
