fe05df9e2ddefe8a65b30ad7327e509121754385
leocassiosilva <leocassiosilva1234@gmail.com>
2021-03-30 22:42:33 -0300

    inciando a listagem de pedidos do cliente

--- vendedor/urls.py
+++ vendedor/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, re_path
-from .views import VendedoresListView
+from .views import VendedoresListView, MinhasVendasListView
 
 urlpatterns = [
     path('bancas/', VendedoresListView.as_view(), name='vendedor_banca'),
-
+    path('vendas/', MinhasVendasListView.as_view(), name='vendedor_vendas'),
 ]
--- vendedor/views.py
+++ vendedor/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.views.generic import ListView
 
 from accounts.models import CustomUsuario
+from pedidos.models import Pedido, PedidoItem
 from product.models import Product
 from projetoIntegrador.roles import Vendedor
 
@@ -18,4 +19,23 @@ class VendedoresListView(ListView):
         return vendedor
 
 
+class MinhasVendasListView(ListView):
+    template_name = 'vendedor/vendas_list.html'
+    model = Pedido
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+
+        produto = [produto.id for produto in Product.objects.filter(id_usuario=self.request.user)]
+        print(produto)
+
+        pd = PedidoItem.objects.filter(product__in=produto)
+
+        print(pd)
+        pedido = [pedido for pedido in Pedido.objects.all()]
+
+        pedidos_items = list(PedidoItem.objects.filter(pedido__in=pedido))
+        context['pedidos_items'] = pedidos_items
+        print(pedidos_items)
+        return context
