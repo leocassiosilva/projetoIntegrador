8435393f2c36d49572c29e747e87bfed554147f0
leocassiosilva <leocassiosilva1234@gmail.com>
2021-04-06 20:37:50 -0300

    download do relatorio de pedidos do mes

--- relatorio/urls.py
+++ relatorio/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
-from .views import GeneratePdf
+from .views import GeneratePdf,TemplateRelatorio
 
 urlpatterns = [
     path('produtos/', GeneratePdf.as_view(), name='gerar_relatorio'),
-
+    path('baixar/', TemplateRelatorio.as_view(), name='baixar_relatorio'),
 ]
--- relatorio/views.py
+++ relatorio/views.py
@@ -1,18 +1,83 @@
+from PyPDF2.pdf import BytesIO
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
-from django.views.generic.base import View
+from django.template.loader import get_template
+from django.views.generic.base import View, TemplateView
+from reportlab.pdfgen import canvas
+from rolepermissions.mixins import HasRoleMixin
 
+from pedidos.models import PedidoItem, Pedido
 from product.models import Product
 from projetoIntegrador.utils import render_to_pdf  # created in step 4
 
 
+class TemplateRelatorio(LoginRequiredMixin, HasRoleMixin, TemplateView):
+    template_name = 'produtos/baixar_relatorio.html'
+    allowed_roles = 'vendedor'
+
+
 class GeneratePdf(View):
+
     def get(self, request, *args, **kwargs):
-        produtos = Product.objects.order_by('name').filter(id_usuario=self.request.user)
-        data = {'produtos': produtos}
+        now = datetime.now()
+        produto = Product.objects.filter(id_usuario=request.user.id)
+        pedidos_mes = Pedido.objects.filter(data_criacao__month=now.month)
+        produtos = PedidoItem.objects.filter(product__in=produto, pedido__in=pedidos_mes).values('product__name',
+                                                                                                 'product__price',
+                                                                                                 'pedido__id').annotate(
+            Sum('quantidade'))
+        data_atual = datetime.now()
+        data = {'produtos': produtos, 'data': data_atual}
 
         pdf = render_to_pdf('produtos/relatorio.html', data)
         return HttpResponse(pdf, content_type='application/pdf')
 
+
+class GeneratePDF(View):
+    def get(self, request, *args, **kwargs):
+        template = get_template('produtos/relatorio.html')
+
+        context = {
+            "invoice_id": 123,
+            "customer_name": "John Cooper",
+            "amount": 1399.99,
+            "today": "Today",
+        }
+        html = template.render(context)
+        pdf = render_to_pdf('produtos/relatorio.html', context)
+        if pdf:
+            response = HttpResponse(pdf, content_type='application/pdf')
+            filename = "Invoice_%s.pdf" % ("12341231")
+            content = "inline; filename='%s'" % (filename)
+            download = request.GET.get("download")
+            if download:
+                content = "attachment; filename='%s'" % (filename)
+            response['Content-Disposition'] = content
+            return response
+        return HttpResponse("Not found")
+
+
+def write_pdf_view(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'inline; filename="mypdf.pdf"'
+
+    buffer = BytesIO()
+    p = canvas.Canvas(buffer)
+
+    # Start writing the PDF here
+    p.drawString(100, 100, 'Hello world.')
+    # End writing
+
+    p.showPage()
+    p.save()
+
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
+
+    return response
