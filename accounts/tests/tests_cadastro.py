7a11b552fb69e552656798ce87200498771facfb
leocassiosilva <leocassiosilva1234@gmail.com>
2021-04-07 08:23:38 -0300

    arrumando alguns testes

--- accounts/tests/tests_cadastro.py
+++ accounts/tests/tests_cadastro.py
@@ -1,31 +1,35 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 
+from accounts.models import CustomUsuario
+
+
 class CadastraViewTesteCase(TestCase):
 
     def setUp(self):
         self.client = Client()
         self.register_url = reverse('cadastrar')
 
-    '''
-
-    def test_cadastro_ok(self):
-        data = {'username': 'francisco@gmail.com',
+    def test_register_ok(self):
+        data = {'username': 'leocassio@gmail.com',
                 'first_name': 'Francisco',
                 'last_name': 'Leocassio',
                 'telefone': '(84)00000-0000',
                 'cep': '654454',
                 'cidade': 'Natal',
                 'rua': 'Das Almas',
+                'nome_banca': 'Banca Boa',
                 'bairro': 'Centro',
                 'logadouro': 'Casa',
                 'numero': 'S/N',
                 'password': 'mnbvcxz987654321'}
+
         response = self.client.post(self.register_url, data)
-        self.assertRedirects(response, 'login/')
+        print(response)
+        index_url = reverse('login')
+        self.assertRedirects(response, index_url)
         self.assertEquals(CustomUsuario.objects.count(), 1)
 
-'''
 
     def test_cadastro_error(self):
         data = {'username': '',
@@ -42,4 +46,3 @@ class CadastraViewTesteCase(TestCase):
                 'password': 'mnbvcxz987654321'}
         response = self.client.post(self.register_url, data)
         self.assertFormError(response, 'form', 'username', 'Este campo é obrigatório.')
-
--- product/tests/tests_models.py
+++ product/tests/tests_models.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 from model_mommy import mommy
 from product.models import Product
 
@@ -6,9 +7,10 @@ from product.models import Product
 class ProductTestCase(TestCase):
 
     def setUp(self):
-        self.product = mommy.make(Product)
+        self.product = mommy.make(Product, slug='produto')
 
     def test_get_absolute_url(self):
-        self.assertTrue(Product(self.product, Product))
-        self.assertEquals(self.product.__str__(), self.product.name)
-
+        self.assertEquals(
+            self.product.get_absolute_url(),
+            reverse('product:product', kwargs={'slug': self.category.slug})
+        )
